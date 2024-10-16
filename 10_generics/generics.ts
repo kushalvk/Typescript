08ea23b08352d17336f8a95b10635509b90d6811
Kushal Vaghela <142973474+kushalvk@users.noreply.github.com>
2024-10-16 12:19:29 +0000

    generics

--- 10_generics/generics.ts
+++ 10_generics/generics.ts
@@ -16,8 +16,8 @@ function identityThree<Type>(val: Type): Type {
     return val
 }
 //  in this :- 1) if the accept type is integer the return and other type will assign automaticaly integer
-            // 2) if the accept type is String the return and other type will assign automaticaly String 
-            // and any other datatype also
+// 2) if the accept type is String the return and other type will assign automaticaly String 
+// and any other datatype also
 
 identityThree("3") // hover on that (lock the datatype)
 
@@ -27,9 +27,70 @@ function identityFour<A>(val: A): A {
 }
 
 // you can use also like that
-interface bootle{
+interface bootle {
     brand: string,
     type: number
 }
 
 // identityFour<bootle>({})
+
+
+// Generics in Array
+function getSearchProduct<T>(product: T[]): T {
+    // do some database opration
+    const myIndex = 3
+    return product[myIndex]
+}
+
+
+// generics arrow function
+const getMoreSeachProducts = <T>(product: T[]): T => {
+    // do some database opration
+    const myIndex = 4
+    return product[myIndex]
+}
+
+// <T,> -> it mean it is not any tag it is a generics 
+// mostly it use in the '.jsx' & '.tsx' file
+
+
+// generic classes
+
+interface Database {
+    connection: string,
+    username: string,
+    password: string
+}
+
+function anotherFunction<T, U extends Database>(valOne: T, valTwo: U): object {
+    return {
+        valOne,
+        valTwo
+    }
+}
+
+// anotherFunction(3, 4) // you can compalsary a second paramitar is number if extend number.
+anotherFunction(3, {
+    connection: "yes",
+    username: "kushal",
+    password: "1234567"
+}) // in this there is object
+
+
+interface Quiz {
+    name: string,
+    type: string
+}
+interface Cource {
+    name: string,
+    author: string,
+    subject: string
+}
+
+class Sellable<T>{
+    public cart: T[] = []
+
+    addToCart(product: T) {
+        this.cart.push(product)
+    }
+}
